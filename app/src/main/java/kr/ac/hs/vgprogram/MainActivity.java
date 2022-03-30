c76ea7596fbc061fc30a26f5b0c2c42a16992999
janghn <gkslcjstksla@gamil.com>
2022-03-30 21:39:27 +0900

    first commit

--- app/src/main/java/kr/ac/hs/vgprogram/MainActivity.java
+++ app/src/main/java/kr/ac/hs/vgprogram/MainActivity.java
@@ -3,6 +3,8 @@ package kr.ac.hs.vgprogram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button login_button = findViewById(R.id.login_button);//로그인 버튼
+        Button join_button = findViewById(R.id.join_button);//회원가입 버튼
+        Button id_button = findViewById(R.id.id_button);//아이디찾기 버튼
+        Button passwd_button = findViewById(R.id.passwd_button);//비밀번호찾기 버튼
+
+        //로그인 버튼 클릭할 경우
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        //회원가입 버튼 클릭할경우
+        join_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        //아이디찾기 버튼 클릭할경우
+        id_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        //비밀번호찾기 버튼 클릭할경우
+        passwd_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
